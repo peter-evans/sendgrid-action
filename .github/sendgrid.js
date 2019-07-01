7a95bb55172a81b2686eab71069e0462fc566004
Peter Evans <peter-evans@users.noreply.github.com>
2019-07-01 20:29:50 +0900

    Update script

--- .github/sendgrid.js
+++ .github/sendgrid.js
@@ -2,13 +2,28 @@
 
 const sgMail = require('@sendgrid/mail');
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+const fs = require('fs'),
+    filename = 'hello-world.pdf',
+    fileType = 'application/pdf',
+    data = fs.readFileSync('attachments/' + filename);
+
 const msg = {
     to: 'peterevans.dev@protonmail.com',
     from: 'sender@example.org',
     subject: 'Hello world',
     text: 'Hello plain world!',
     html: '<p>Hello HTML world!</p>',
+    attachments: [
+        {
+            content: data.toString('base64'),
+            filename: filename,
+            type: fileType,
+            disposition: 'attachment',
+        },
+    ],
 };
+
 sgMail
     .send(msg)
     .then(() => console.log('Mail sent successfully'))
